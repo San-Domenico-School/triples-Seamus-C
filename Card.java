a03ddf789353944a22896fac59c4dba0d03c7eae
Seamus-C <scrain26@sandomenico.org>
2024-01-08 11:06:09 -0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -9,6 +9,59 @@ import greenfoot.*;
 
 public class Card extends Actor
 {
-   
+    private boolean isSelected;
+    
+    private Shape shape;
+    private Color color;
+    private int numberOfShapes;
+    private int shading;
+    
+    private GreenfootImage cardImage, selectedCardImage;
+    
+    public enum Shape
+    {
+        TRIANGLE, SQUARE, CIRCLE, NO_SHAPE
+    }
+    public enum Color
+    {
+        RED, GREEN, BLUE, NO_COLOR
+    }
+    
+    public Card(Shape shape, Color color, int numberOfShapes, int shading, GreenfootImage cardImage, GreenfootImage selectedCardImage)
+    {
+        this.shape = shape;
+        this.color = color;
+        this.numberOfShapes = numberOfShapes;
+        this.shading = shading;
+        this.cardImage = cardImage;
+        this.selectedCardImage = selectedCardImage;
+        
+        setImage(cardImage);
+    }
+    
+    public Shape getShape()
+    {
+        return shape;
+    }
+    public Color getColor()
+    {
+        return color;
+    }
+    public int getNumberOfShapes()
+    {
+        return numberOfShapes;
+    }
+    public int getShading()
+    {
+        return shading;
+    }
+    public boolean getSelected()
+    {
+        return isSelected;
+    }
+    public void setSelected(boolean selected)
+    {
+        isSelected = selected;
+    }
 }
 
